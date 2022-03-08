d890470ce8a9b8b55618471f869c89455be59c06
Beknaza <bajnazarovbeknazar@gmail.com>
2022-03-08 18:02:18 +0600

    Dz4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,9 @@ public class Main {
     public static String bossDefenceType;
     public static int bossDamage = 50;
     public static int bossHealth = 700;
-    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic", "Medic"};
-    public static int[] heroesDamage = {25, 20, 15, 0};
-    public static int[] heroesHealth = {340, 270, 250, 300};
+    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic", "Medic", "Golem", "Lucky", "Berserk", "Thor"};
+    public static int[] heroesDamage = {25, 20, 15, 0, 5, 10, 5, 5};
+    public static int[] heroesHealth = {340, 270, 250, 300, 400, 200, 250, 200};
     public static int roundNumber = 0;
 
     public static void main(String[] args) {
@@ -21,23 +21,90 @@ public class Main {
     public static void round() {
         roundNumber++;
         bossDefenceType = chooseDefence();
+        Thor();
         bossHits();
         heroesHit();
         medic();
+        Golem();
+        Lucky();
+        Berserk();
         printStatistics();
     }
-    public static void medic (){
-        for (int i = 0; i <heroesHealth.length; i++) {
-            if (heroesHealth[i]>0 && heroesHealth[i]<100 && heroesHealth[3]>0) {
-                if (heroesHealth[i] == heroesHealth[3]){
+
+    public static void medic() {
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (heroesHealth[i] > 0 && heroesHealth[i] < 100 && heroesHealth[3] > 0) {
+                if (heroesHealth[i] == heroesHealth[3]) {
                     continue;
                 }
-                heroesHealth[i]+=50;
-                System.out.println(heroesAttackType[i]+" medic вылечил");
+                heroesHealth[i] += 50;
+                System.out.println(heroesAttackType[i] + " medic вылечил");
                 break;
             }
         }
     }
+
+    public static void Golem() {
+        int dmg = bossDamage / 5;
+        int gg = 0;
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (i == 4) {
+                continue;
+            } else if (heroesHealth[i] > 0) {
+                gg++;
+                heroesHealth[i] += dmg;
+            }
+            heroesHealth[4] -= dmg * gg;
+            System.out.println("Golem took damage " + dmg * gg);
+            break;
+        }
+        if (heroesHealth[4] <= 0) {
+            heroesHealth[4] = 0;
+        }
+    }
+
+    public static void Lucky() {
+        Random random = new Random();
+        boolean aqd = random.nextBoolean();
+        if (heroesHealth[5] > 0 && aqd) {
+            heroesHealth[5] += bossDamage - bossDamage / 5 + 10;
+        }
+        System.out.println("");
+
+    }
+
+    {
+    }
+
+    public static void Thor() {
+        Random random = new Random();
+        boolean oglushka = random.nextBoolean();
+        if (heroesHealth[7] > 0 && oglushka) {
+            bossDamage = 0;
+            System.out.println("Thor оглушил");
+        } else {
+            bossDamage = 50;
+        }
+    }
+
+    {
+    }
+
+    public static void Berserk() {
+        for (int i = 0; i < heroesHealth.length ; i++) {
+            if (heroesHealth[6] > 0) {
+                heroesHealth[6] += bossDamage / 5 * 2;
+                bossHealth -= bossDamage / 5 * 2;
+                break;
+            }else {
+                bossDamage = 50;
+            }
+        }
+    }
+
+    {
+    }
+
 
     public static String chooseDefence() {
         Random random = new Random();
@@ -106,6 +173,7 @@ public class Main {
 
     public static void printStatistics() {
         System.out.println(roundNumber + " ROUND _________________");
+
         System.out.println("Boss health: " + bossHealth + " (" + bossDamage + ")");
         for (int i = 0; i < heroesHealth.length; i++) {
             System.out.println(heroesAttackType[i] + " health: " +
